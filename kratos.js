f732e43ad89e89a77e18a5969134845be82e0b9d
Devin Leaman <dleaman212@gmail.com>
2018-10-24 15:17:08 -0500

    Updated kratos.js to use new method locations

--- kratos.js
+++ kratos.js
@@ -3,73 +3,46 @@ const { google } = require('googleapis')
 
 let auth = config.youtubeKey
 
+const channel = new (require('./util/channel'))(auth)
+const playlists = new (require('./util/playlists'))(auth)
+
 const service = google.youtube('v3')
 
 const fs = require('fs')
 const path = require('path')
+
+const getAllVideos = async (playlistId, maxResults = undefined, pageToken = undefined) => {
+  let res = await service.playlistItems.list({
+    playlistId: playlistId,
+    auth: auth,
+    part: 'snippet,contentDetails',
+    maxResults: maxResults,
+    pageToken: pageToken
+  })
+}
 
 service.playlistItems.list({
   playlistId: config.ggUploadPlaylistId,
   auth: auth,
   part: 'snippet,contentDetails'
 }, (err, res) => {
-  if (err) {
-    console.error(err)
-    return
+  if (err) console.error(err)
+  else {
+    let videoIds = []
+
+    for (let x = 0; x < 5; x++) { videoIds.push(res.data.items[x].videoId) }
+
+    playlists.getVideos(config.ggUploadPlaylistId, res.data.nextPageToken).then(res => {
+      console.log(videoIds.length)
+      console.log(res.length)
+
+    // fs.writeFile(path.join(__dirname, 'output.txt'), videoIds, err => {
+    //   if (err) console.error(err)
+    //   else console.log(`File written to disc.`)
+    // })
+    }).catch(err => console.error(err))
   }
-  let videoIds = []
-
-  for (let x = 0; x < 5; x++) {
-    videoIds.push(res.data.items[x].videoId)
-  }
-
-  getVideos(config.ggUploadPlaylistId, res.data.nextPageToken).then(res => {
-    videoIds.push(res)
-
-    console.log(videoIds.length)
-
-    fs.writeFile(path.join(__dirname, 'output.txt'), videoIds, err => {
-      if (err) console.error(err)
-      else console.log(`File written to disc.`)
-    })
-  }).catch(err => console.error(err))
 })
-
-const getVideos = async (playlistId, pageToken) => {
-  let videoIds = []
-  let pageNum = 0
-
-  while (pageToken !== undefined) {
-    console.log(`Listing page ${pageNum}`)
-
-    let items = await service.playlistItems.list({
-      auth: auth,
-      part: 'snippet,contentDetails',
-      playlistId: playlistId,
-      pageToken: pageToken
-    })
-
-    pageToken = items.data.nextPageToken
-
-    for (let x = 0; x < 5; x++) {
-      videoIds.push(items.data.items[x].contentDetails.videoId)
-    }
-
-    pageNum++
-  }
-
-  return videoIds
-}
-
-const getChannelId = async channelName => {
-  let response = await service.channels.list({
-    auth: auth,
-    part: 'id',
-    forUsername: channelName
-  })
-
-  return response.data.items[0].id
-}
 
 // const getPlaylistId = async playlistName => {
 //   let response = await service.playlists.list({
