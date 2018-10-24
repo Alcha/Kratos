7bc95dcbed1071f5a18f628c787fe507adc8298c
Alcha <dleaman212@gmail.com>
2018-10-23 23:21:18 -0500

    Added base files

---
+++ kratos.js
@@ -0,0 +1,92 @@
+const config = require('./config.json')
+const { google } = require('googleapis')
+
+let auth = config.youtubeKey
+
+const service = google.youtube('v3')
+// service.channels.list({
+//   auth: auth,
+//   part: 'snippet,contentDetails,statistics',
+//   forUsername: 'GameGrumps'
+// }, (err, res) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+
+//   let channels = res.data.items
+//   if (channels.length === 0) console.log(`No channels found.`)
+//   else {
+//     console.log(`This channel ID is ${channels[0].id}`)
+//     console.log(channels[0].contentDetails.relatedPlaylists)
+//   }
+// })
+
+const fs = require('fs')
+const path = require('path')
+
+service.playlistItems.list({
+  playlistId: config.ggUploadPlaylistId,
+  auth: auth,
+  part: 'snippet,contentDetails'
+}, (err, res) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  let videoIds = []
+
+  for (let x = 0; x < 5; x++) {
+    videoIds.push(res.data.items[x].videoId)
+  }
+
+  getVideos(config.ggUploadPlaylistId, res.data.nextPageToken).then(res => {
+    videoIds.push(res)
+
+    console.log(videoIds.length)
+
+    fs.writeFile(path.join(__dirname, 'output.txt'), videoIds, err => {
+      if (err) console.error(err)
+      else console.log(`File written to disc.`)
+    })
+  }).catch(err => console.error(err))
+})
+
+// service.playlists.list({
+//   auth: auth,
+//   id: config.ggUploadChannel,
+//   part: 'snippet,contentDetails'
+// }, (err, res) => {
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
+
+//   console.log(res.data.items[0])
+// })
+
+const getVideos = async (playlistId, pageToken) => {
+  let videoIds = []
+  let pageNum = 0
+
+  while (pageToken !== undefined) {
+    console.log(`Listing page ${pageNum}`)
+
+    let items = await service.playlistItems.list({
+      auth: auth,
+      part: 'snippet,contentDetails',
+      playlistId: playlistId,
+      pageToken: pageToken
+    })
+
+    pageToken = items.data.nextPageToken
+
+    for (let x = 0; x < 5; x++) {
+      videoIds.push(items.data.items[x].contentDetails.videoId)
+    }
+
+    pageNum++
+  }
+
+  return videoIds
+}
